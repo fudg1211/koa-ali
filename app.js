a71d854b4cadb16eebb9feef99247ab3d00b1aea
fdg <fdg@fdgdeMacBook-Pro.local>
2021-02-25 16:31:54 +0800

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: huajian
+ * @LastEditors: huajian
+ */
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
